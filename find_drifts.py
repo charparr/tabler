35f0c5de1f0dd7eebaf6f69e0491e82a19b7025f
Charlie Parr <charlie@arcticfiredevelopment.com>
2017-06-13 13:49:28 -0800

    find drifts and grab properties to df

--- find_drifts.py
+++ find_drifts.py
@@ -13,16 +13,15 @@ continuous depth above some threshold, i.e. one sigma above the mean.
 
 import numpy as np
 import matplotlib.pyplot as plt
-import warnings
-from math import factorial
 from skimage import io
-from skimage.measure import profile_line
 from scipy.ndimage import gaussian_filter
-from scipy import integrate
 import scipy.ndimage
-import matplotlib.patches as patches
-from astropy.convolution import convolve_fft
-# Global Methods
+from skimage.measure import regionprops
+from skimage.measure import label
+from skimage.measure import find_contours
+from skimage.morphology import remove_small_objects
+from skimage.morphology import remove_small_holes
+import pandas as pd
 
 
 
@@ -72,203 +71,135 @@ class Surface(object):
                 l_edge = np.where(np.array(nd_boundaries) > 1)[0][0]
                 r_edge = l_edge + max(nd_boundaries)
                 self.subdict[nam] = self.subdict[nam][::,l_edge:r_edge]
-                
-#class Surface_Derivs(object):
-#
-#    def __init__(self,surf):
-#        self.surf = surf
-#        
-#        y = self.surf.copy()
-#        window_size = 5
-#        order = 1
-#        rate = 1
-#        deriv = 1
-#        
-#        order_range = range(order+1)
-#        half_window = (window_size -1) // 2
-#        # precompute coefficients
-#        b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
-#        m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
-#        # pad the signal at the extremes with
-#        # values taken from the signal itself
-#        firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
-#        lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
-#        y = np.concatenate((firstvals, y, lastvals))
-#        self.slope =  np.convolve( m[::-1], y, mode='valid')
-        
-def sgolay2d ( z, window_size, order, derivative=None):
-    """
-    """
-    # number of terms in the polynomial expression
-    n_terms = ( order + 1 ) * ( order + 2)  / 2.0
-
-    if  window_size % 2 == 0:
-        raise ValueError('window_size must be odd')
-
-    if window_size**2 < n_terms:
-        raise ValueError('order is too high for the window size')
-
-    half_size = window_size // 2
-
-    # exponents of the polynomial. 
-    # p(x,y) = a0 + a1*x + a2*y + a3*x^2 + a4*y^2 + a5*x*y + ... 
-    # this line gives a list of two item tuple. Each tuple contains 
-    # the exponents of the k-th term. First element of tuple is for x
-    # second element for y.
-    # Ex. exps = [(0,0), (1,0), (0,1), (2,0), (1,1), (0,2), ...]
-    exps = [ (k-n, n) for k in range(order+1) for n in range(k+1) ]
-
-    # coordinates of points
-    ind = np.arange(-half_size, half_size+1, dtype=np.float64)
-    dx = np.repeat( ind, window_size )
-    dy = np.tile( ind, [window_size, 1]).reshape(window_size**2, )
-
-    # build matrix of system of equation
-    A = np.empty( (window_size**2, len(exps)) )
-    for i, exp in enumerate( exps ):
-        A[:,i] = (dx**exp[0]) * (dy**exp[1])
-
-    # pad input array with appropriate values at the four borders
-    new_shape = z.shape[0] + 2*half_size, z.shape[1] + 2*half_size
-    Z = np.zeros( (new_shape) )
-    # top band
-    band = z[0, :]
-    Z[:half_size, half_size:-half_size] =  band -  np.abs( np.flipud( z[1:half_size+1, :] ) - band )
-    # bottom band
-    band = z[-1, :]
-    Z[-half_size:, half_size:-half_size] = band  + np.abs( np.flipud( z[-half_size-1:-1, :] )  -band )
-    # left band
-    band = np.tile( z[:,0].reshape(-1,1), [1,half_size])
-    Z[half_size:-half_size, :half_size] = band - np.abs( np.fliplr( z[:, 1:half_size+1] ) - band )
-    # right band
-    band = np.tile( z[:,-1].reshape(-1,1), [1,half_size] )
-    Z[half_size:-half_size, -half_size:] =  band + np.abs( np.fliplr( z[:, -half_size-1:-1] ) - band )
-    # central band
-    Z[half_size:-half_size, half_size:-half_size] = z
-
-    # top left corner
-    band = z[0,0]
-    Z[:half_size,:half_size] = band - np.abs( np.flipud(np.fliplr(z[1:half_size+1,1:half_size+1]) ) - band )
-    # bottom right corner
-    band = z[-1,-1]
-    Z[-half_size:,-half_size:] = band + np.abs( np.flipud(np.fliplr(z[-half_size-1:-1,-half_size-1:-1]) ) - band )
-
-    # top right corner
-    band = Z[half_size,-half_size:]
-    Z[:half_size,-half_size:] = band - np.abs( np.flipud(Z[half_size+1:2*half_size+1,-half_size:]) - band )
-    # bottom left corner
-    band = Z[-half_size:,half_size].reshape(-1,1)
-    Z[-half_size:,:half_size] = band - np.abs( np.fliplr(Z[-half_size:, half_size+1:2*half_size+1]) - band )
-
-    # solve system and convolve
-    if derivative == None:
-        m = np.linalg.pinv(A)[0].reshape((window_size, -1))
-        return convolve_fft(Z, m, interpolate_nan=True)
-    elif derivative == 'col':
-        c = np.linalg.pinv(A)[1].reshape((window_size, -1))
-        return convolve_fft(Z, -c)
-    elif derivative == 'row':
-        r = np.linalg.pinv(A)[2].reshape((window_size, -1))
-        return convolve_fft(Z, -r)
-    elif derivative == 'both':
-        c = np.linalg.pinv(A)[1].reshape((window_size, -1))
-        r = np.linalg.pinv(A)[2].reshape((window_size, -1))
-        return convolve_fft(Z, -r), convolve_fft(Z,-c)
-    
-#    def savitzky_golay(self, y, window_size, order, deriv=0, rate=1):
-#         """Smooth (and optionally differentiate) with a Savitzky-Golay filter.
-#         The filter removes high frequency noise from data.
-#         It has the advantage of preserving the original shape and
-#         features of the signal better than other types of filtering
-#         approaches, such as moving averages techniques.
-#         Parameters
-#         ----------
-#         y : array_like, shape (N,)
-#             the values of the time history of the signal.
-#         window_size : int
-#             the length of the window. Must be an odd integer number.
-#         order : int
-#             the order of the polynomial used in the filtering.
-#             Must be less then `window_size` - 1.
-#         deriv: int
-#             order of the derivative to compute (default = 0 is only smoothing)
-#         Returns
-#         -------
-#         ys : ndarray, shape (N)
-#             the smoothed signal (or it's n-th derivative).
-#         Notes
-#         -----
-#         The Savitzky-Golay is a type of low-pass filter, particularly
-#         suited for smoothing noisy data. The main idea behind this
-#         approach is to make for each point a least-square fit with a
-#         polynomial of high order over a odd-sized window centered at
-#         the point.
-#         Examples
-#         --------
-#         t = np.linspace(-4, 4, 500)
-#         y = np.exp( -t**2 ) + np.random.normal(0, 0.05, t.shape)
-#         ysg = savitzky_golay(y, window_size=31, order=4)
-#         import matplotlib.pyplot as plt
-#         plt.plot(t, y, label='Noisy signal')
-#         plt.plot(t, np.exp(-t**2), 'k', lw=1.5, label='Original signal')
-#         plt.plot(t, ysg, 'r', label='Filtered signal')
-#         plt.legend()
-#         plt.show()
-#         References
-#         ----------
-#         .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
-#            Data by Simplified Least Squares Procedures. Analytical
-#            Chemistry, 1964, 36 (8), pp 1627-1639.
-#         .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
-#            W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
-#            Cambridge University Press ISBN-13: 9780521880688
-#         """
-#     
-#         try:
-#             window_size = np.abs(np.int(window_size))
-#             order = np.abs(np.int(order))
-#         except:
-#             raise ValueError("window_size and order have to be of type int")
-#         if window_size % 2 != 1 or window_size < 1:
-#             raise TypeError("window_size size must be a positive odd number")
-#         if window_size < order + 2:
-#             raise TypeError("window_size is too small for the polynomials order")
-#         order_range = range(order+1)
-#         half_window = (window_size -1) // 2
-#         # precompute coefficients
-#         b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
-#         m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
-#         # pad the signal at the extremes with
-#         # values taken from the signal itself
-#         firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
-#         lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
-#         y = np.concatenate((firstvals, y, lastvals))
-#         return np.convolve( m[::-1], y, mode='valid')    
 
 years = ['2012','2013','2015','2016']
-  
+ 
+# 11360 x 2340 
 bare_surf = Surface('/home/cparr/surfaces/level_1_surfaces/hv/bare_earth/hv_2012_158_bare_earth_dem.tif')
+slope_surf = Surface('/home/cparr/surfaces/slope_surfaces/hv_slope.tif')
+curve_surf = Surface('/home/cparr/surfaces/slope_surfaces/hv_curve.tif')
+
 snow_depth_surf16 = Surface('/home/cparr/surfaces/depth_ddems/hv/hv_2016_096_depth.tif')
 snow_depth_surf15 = Surface('/home/cparr/surfaces/depth_ddems/hv/hv_2015_096_depth.tif')
 snow_depth_surf13 = Surface('/home/cparr/surfaces/depth_ddems/hv/hv_2013_103_depth.tif')
+
+#11358 x 2340
 snow_depth_surf12 = Surface('/home/cparr/surfaces/depth_ddems/hv/hv_2012_107_depth.tif')
-                
-bare_surf.subset_surf(12, ['b1','b2','b3','b4',
-                           'b5','b6','b7','b8',
-                           'b9','b10','b11','b12'])
+
+# 11356 x 2340
+slope_w12 = Surface('/home/cparr/surfaces/slope_surfaces/hv_w12_slope_clip.tif')
+# 11358 x 2340
+slope_w13 = Surface('/home/cparr/surfaces/slope_surfaces/hv_w13_slope_clip.tif')
+# 11358 x 2340
+slope_w15 = Surface('/home/cparr/surfaces/slope_surfaces/hv_w15_slope_clip.tif')
+# 11360 x 2340
+slope_w16 = Surface('/home/cparr/surfaces/slope_surfaces/hv_w16_slope_clip.tif')
+
+all_surfs = [bare_surf,slope_surf,curve_surf,snow_depth_surf12,
+             snow_depth_surf13,snow_depth_surf15,snow_depth_surf16,slope_w12,
+             slope_w13,slope_w15,slope_w16]
+
+for s in all_surfs:
     
-snow_depth_surf16.subset_surf(12, ['2016_1','2016_2','2016_3','2016_4',
-                                 '2016_5','2016_6','2016_7','2016_8',
-                                 '2016_9','2016_10','2016_11','2016_12'])
+    diff = s.arr.shape[0]-11356
+    s.arr = s.arr[diff:]
+    print (s.arr.shape)
+
+mean_wslope = (slope_w12.arr+slope_w13.arr+slope_w15.arr+slope_w16.arr)/4
+
+mean_depth = (snow_depth_surf12.arr+snow_depth_surf13.arr+
+                        snow_depth_surf15.arr+snow_depth_surf16.arr)/4
+              
+# find _drifts
+step1 = gaussian_filter(mean_depth,sigma=2)
+step2 = (step1 > np.nanmean(step1)+np.nanstd(step1))
+step3  = remove_small_objects(step2)
+step4  = remove_small_holes(step3)
+step5 = step4 * 1
+drifts = step4 * mean_depth
+
+
+contours = find_contours(step5,0.5)
+
+lab_img = label(step4)
+
+snow_props = regionprops(lab_img,mean_depth)
+wslope_props = regionprops(lab_img, mean_wslope)
+slope_props = regionprops(lab_img, slope_surf.arr)
+
+i=0
+idx = []
+for r in snow_props:
+    r.name = 'drift_' + str(i)
+    idx.append(r.name)
+    i += 1
+
+i=0
+for r in slope_props:
+    r.name = 'drift_' + str(i)
+    i += 1
     
-snow_depth_surf15.subset_surf(12, ['2015_1','2015_2','2015_3','2015_4',
-                                 '2015_5','2015_6','2015_7','2015_8',
-                                 '2015_9','2015_10','2015_11','2015_12'])
+i=0
+for r in wslope_props:
+    r.name = 'drift_' + str(i)
+    i += 1
+
+# Dump Results to DataFrame
+
+snowfields = ['area', 'bbox', 'centroid', 'convex_area',
+       'convex_image', 'coords', 'eccentricity',
+       'equivalent_diameter', 'euler_number', 'extent',
+       'image', 'label',
+       'major_axis_length', 'max_intensity', 'mean_intensity',
+       'min_intensity', 'minor_axis_length',
+       'moments_hu', 'orientation', 'perimeter',
+       'solidity', 'weighted_centroid','weighted_moments_hu']
+
+slopefields=['slope max_intensity', 'slope mean_intensity',
+             'slope min_intensity','slope weighted_centroid',
+             'slope weighted_moments_hu']
+
+wslopefields=['wslope max_intensity', 'wslope mean_intensity',
+             'wslope min_intensity','wslope weighted_centroid',
+             'wslope weighted_moments_hu']
+
+df = pd.DataFrame(index=idx, columns = snowfields+slopefields+wslopefields)
+
+
+for r in snow_props:
+    for c in snowfields:
+            df.ix[r.name][c]=r[c]
+for r in slope_props:
+    for c in slopefields:
+        n = c[6:]
+        df.ix[r.name][c]=r[n]
+for r in wslope_props:
+    for c in wslopefields:
+        n = c[7:]
+        df.ix[r.name][c]=r[n]
+
+for j in range(7):
+    df['moments_hu'+str(j)]=[i[j] for i in df['moments_hu']]
+    df['weighted_moments_hu'+str(j)]=[i[j] for i in df['weighted_moments_hu']]
+    df['slope weighted_moments_hu'+str(j)]=[i[j] for i in df['slope weighted_moments_hu']]
+    df['wslope weighted_moments_hu'+str(j)]=[i[j] for i in df['wslope weighted_moments_hu']]
+
+del df['moments_hu'],df['weighted_moments_hu'],df['slope weighted_moments_hu'],df['wslope weighted_moments_hu']
+df.columns
+
+
+
+
+
+#bigdf = df[df['area']>500]
+#
+fig, ax = plt.subplots()
+ax.imshow(mean_depth, interpolation='nearest', cmap='gray',vmin=0,vmax=2)
+
+for n, reg in enumerate(snow_props):
+    ax.plot(reg.coords[:, 1], reg.coords[:, 0], linewidth=1,alpha=0.5,c='m')
+    #ax.text(reg.centroid[1], reg.centroid[0], reg.name)
+plt.show()
     
-snow_depth_surf13.subset_surf(12, ['2013_1','2013_2','2013_3','2013_4',
-                                 '2013_5','2013_6','2013_7','2013_8',
-                                 '2013_9','2013_10','2013_11','2013_12'])
-    
-snow_depth_surf12.subset_surf(12, ['2012_1','2012_2','2012_3','2012_4',
-                                 '2012_5','2012_6','2012_7','2012_8',
-                                 '2012_9','2012_10','2012_11','2012_12'])
+
+
